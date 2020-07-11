3eef0e2e9b8f522085bf6f4d4517e807539f1e91
samjang9509 <samjang9509@github.com>
2020-07-11 12:26:07 +0900

    help

---
+++ build/test/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+  "/home/samuel/workspace/devel/lib/test/listener.pdb"
+  "/home/samuel/workspace/devel/lib/test/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/samuel/workspace/devel/lib/test/talker.pdb"
+  "/home/samuel/workspace/devel/lib/test/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
